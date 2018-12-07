894824d90345c72d7ddda9b7f125eb9b5db49487
https://gist.github.com/ronvigil/TG-Pre-Work <rvgraphicdesign@gmail.com>
2018-12-06 21:54:33 -0700

    reduce returns a single value after iterating thourh an array

--- Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-reduce-Method.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-reduce-Method.js
@@ -1,5 +1,6 @@
 const newNumbers = [1, 3, 5, 7];
 
+
 const newSum = newNumbers.reduce((accumulator, currentValue) => {
   console.log('The value of accumulator: ', accumulator);
   console.log('The value of currentValue: ', currentValue);
