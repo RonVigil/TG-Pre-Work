4b10f8f52171bf50ba0b44e20261aeb0afa37dc5
https://gist.github.com/ronvigil/TG-Pre-Work <rvgraphicdesign@gmail.com>
2018-12-12 21:04:33 -0700

    methods .log() and .floor()

--- Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Methods.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Methods.js
@@ -1,7 +1,6 @@
 let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
 
 // Write your code below
-
 let alienShip = {
   retreat() {
     console.log(retreatMessage)
